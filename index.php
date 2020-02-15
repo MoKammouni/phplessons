450655801366375d081b629e8b45963e76754f77
Mohamed el Kammouni <mokammouni@outlook.com>
2020-02-15 13:58:56 +0100

    Hoop wat documentatie toegevoegd. Op de playlist ben ik Aflevering 12

--- index.php
+++ index.php
@@ -10,12 +10,15 @@
 </head>
 <body>
 
+<!-- Deze opdracht heeft verband tussen commentaar #2
 <form method="get">
     <label>
         <input type="text" name="person">
     </label>
     <button>SUBMIT</button>
 </form>
+-->
+
 <?php
 //In PHP om iets uit te printen op het scherm worden echo en print gebruiken.
 //de echo command is toch sneller dan de print command. in de echo command kan je rekenen
@@ -28,13 +31,49 @@
     //$name = $_GET['person'];
     //echo $name. " is heel cool!"
 
-//De PHP programmeur taal kent 2 soorten functies.
+//De PHP programmeer taal kent 2 soorten functies.
 //Een PHP functie is een stuk PHP code dat een bepaalde opdracht doet.
 // 1. we hebben we voorafbepaalde functies. Dit houd in dat de funtie vooraf gedefineerd
 //Is in de taal.
 
+    //echo strlen("Hamood");
+
 // 2. de gebruiker bepalende functies. Dit is een functie de programmeur zelf schrijft
 //Dus je hem niet kan gebruiken als je hem niet gemaakt hebt.
+
+
+
+//Ik zal nu verschilende datatypes voortonen die plaats vinden in PHP
+//$name = 'Mohamed'; //String. Voor strings is het aangeraden om single quotes te gebruiken.
+//$age = 16;         //Integer. Het verschil tussen integer en float is de punt en de decimalen
+//$height = 1.87;    //Float. Het verschil tussen integer en float is de punt en de decimalen.
+//$student = true;   //Boolean. Een Boolean is wanneer de waarde true of false is.
+//$adres = array('Zuid-Holland',"Den Haag","Molenwijk") //Array. Om iets uit een array te echo moet je de index zeggen in brackets([]).
+
+//Rekenkundig Operatoren
+// +, -, /, * zijn operatoren om te kunnen rekenen met integers.
+// De % operator is te kijken wat er restand is na de integers te hebben gedeeld.
+// De ** is om kwadraat op een integer te kunnen doen
+
+//Vergelijkings operatoren
+// =, ==, ===, !=, !==, <, >, <>, <=, >=
+//Dat wat wij tussen twee variable doen om te vergeleiken zijn vergelijkngs operatoren
+//    $x = 5;
+//    $y = 15;
+
+//    if ($x < $y) {
+//        echo 'True';
+//    }
+//    else {
+//        echo 'false';
+//    }
+
+//Verhogen/Verlagen Operatore.
+//Het nut van deze Operatoren is dat wij een variable met een integer waarde met 1 kunnen verhogen of verlagen
+// ++ (plus 1) moet je altijd voor de variable declaren.
+// -- (min 1) moet je altijd voor de variable declaren.
+
+//Logische operatoren
 ?>
 </body>
 </html>
