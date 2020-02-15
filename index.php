cd1e857ffa6a99b7482a067df9e3bd9e57328c2d
Mohamed el Kammouni <mokammouni@outlook.com>
2020-02-15 01:37:55 +0100

    index.php toegevoegd. Hoop wat documentatie toegevoegd. Op de playlist ben ik Aflevering 6 1:11.

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
+<!--Dit project wordt gehanteerd aandehand van deze playlist: https://www.youtube.com/watch?v=kByr0ifju4k&list=PL0eyrZgxdwhwBToawjm9faF1ixePexft-&index=6 -->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="get">
+    <label>
+        <input type="text" name="person">
+    </label>
+    <button>SUBMIT</button>
+</form>
 <?php
+//In PHP om iets uit te printen op het scherm worden echo en print gebruiken.
+//de echo command is toch sneller dan de print command. in de echo command kan je rekenen
+    //echo 11 + 2;
+
+
+// De benaming van een variable in PHP is een combinatie van de dollar tekenen($) +
+//de naam van de variable. Belangrijk dat je geen spatie tussen doet anders is de variable
+//ongeldig
+    //$name = $_GET['person'];
+    //echo $name. " is heel cool!"
+
+//De PHP programmeur taal kent 2 soorten functies.
+//Een PHP functie is een stuk PHP code dat een bepaalde opdracht doet.
+// 1. we hebben we voorafbepaalde functies. Dit houd in dat de funtie vooraf gedefineerd
+//Is in de taal.
+
+// 2. de gebruiker bepalende functies. Dit is een functie de programmeur zelf schrijft
+//Dus je hem niet kan gebruiken als je hem niet gemaakt hebt.
+?>
+</body>
+</html>
